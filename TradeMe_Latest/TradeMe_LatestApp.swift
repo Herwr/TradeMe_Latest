57a0001edced0748f30c84a972bd72bdfadd6841
Rich <richardwjones@hotmail.com>
2022-02-01 12:05:11 +1300

    Initial Commit

---
+++ TradeMe_Latest/TradeMe_LatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeMe_LatestApp.swift
+//  TradeMe_Latest
+//
+//  Created by Rich on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TradeMe_LatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
